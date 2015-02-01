b681494b9886d505e34df897521ea5d5e9b903f3
msimiste <mikesimister@shaw.ca>
2015-02-01 14:33:57 -0700

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button num1;
+            System.Windows.Forms.Button num3;
+            System.Windows.Forms.Button num2;
+            System.Windows.Forms.Button num4;
+            System.Windows.Forms.Button num5;
+            System.Windows.Forms.Button num6;
+            System.Windows.Forms.Button num8;
+            System.Windows.Forms.Button num9;
+            System.Windows.Forms.Button num0;
+            System.Windows.Forms.Button button7;
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.cmdShift = new System.Windows.Forms.CheckBox();
+            this.cmdCaps = new System.Windows.Forms.CheckBox();
+            num1 = new System.Windows.Forms.Button();
+            num3 = new System.Windows.Forms.Button();
+            num2 = new System.Windows.Forms.Button();
+            num4 = new System.Windows.Forms.Button();
+            num5 = new System.Windows.Forms.Button();
+            num6 = new System.Windows.Forms.Button();
+            num8 = new System.Windows.Forms.Button();
+            num9 = new System.Windows.Forms.Button();
+            num0 = new System.Windows.Forms.Button();
+            button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // num1
+            // 
+            num1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num1.Location = new System.Drawing.Point(76, 36);
+            num1.Name = "num1";
+            num1.Size = new System.Drawing.Size(75, 75);
+            num1.TabIndex = 0;
+            num1.Text = "∮  \r\n∫\r\n";
+            num1.UseVisualStyleBackColor = false;
+            num1.Click += new System.EventHandler(this.num1_Click);
+            // 
+            // num3
+            // 
+            num3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num3.Location = new System.Drawing.Point(226, 36);
+            num3.Name = "num3";
+            num3.Size = new System.Drawing.Size(75, 75);
+            num3.TabIndex = 2;
+            num3.Text = "#\r\n3\r\n";
+            num3.UseVisualStyleBackColor = false;
+            num3.Click += new System.EventHandler(this.num3_Click);
+            // 
+            // num2
+            // 
+            num2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num2.Location = new System.Drawing.Point(151, 36);
+            num2.Name = "num2";
+            num2.Size = new System.Drawing.Size(75, 75);
+            num2.TabIndex = 3;
+            num2.Text = "@\r\n2\r\n";
+            num2.UseVisualStyleBackColor = false;
+            num2.Click += new System.EventHandler(this.num2_Click);
+            // 
+            // num4
+            // 
+            num4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num4.Location = new System.Drawing.Point(301, 36);
+            num4.Name = "num4";
+            num4.Size = new System.Drawing.Size(75, 75);
+            num4.TabIndex = 4;
+            num4.Text = "$\r\n4\r\n";
+            num4.UseVisualStyleBackColor = false;
+            num4.Click += new System.EventHandler(this.num4_Click);
+            // 
+            // num5
+            // 
+            num5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num5.Location = new System.Drawing.Point(376, 36);
+            num5.Name = "num5";
+            num5.Size = new System.Drawing.Size(75, 75);
+            num5.TabIndex = 5;
+            num5.Text = "%\r\n5";
+            num5.UseVisualStyleBackColor = false;
+            num5.Click += new System.EventHandler(this.num5_Click);
+            // 
+            // num6
+            // 
+            num6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num6.Location = new System.Drawing.Point(451, 36);
+            num6.Name = "num6";
+            num6.Size = new System.Drawing.Size(75, 75);
+            num6.TabIndex = 6;
+            num6.Text = "^\r\n6";
+            num6.UseVisualStyleBackColor = false;
+            num6.Click += new System.EventHandler(this.num6_Click);
+            // 
+            // num8
+            // 
+            num8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num8.Location = new System.Drawing.Point(601, 36);
+            num8.Name = "num8";
+            num8.Size = new System.Drawing.Size(75, 75);
+            num8.TabIndex = 8;
+            num8.Text = "*\r\n8";
+            num8.UseVisualStyleBackColor = false;
+            num8.Click += new System.EventHandler(this.num8_Click);
+            // 
+            // num9
+            // 
+            num9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num9.Location = new System.Drawing.Point(676, 36);
+            num9.Name = "num9";
+            num9.Size = new System.Drawing.Size(75, 75);
+            num9.TabIndex = 9;
+            num9.Text = "(\r\n9";
+            num9.UseVisualStyleBackColor = false;
+            num9.Click += new System.EventHandler(this.num9_Click);
+            // 
+            // num0
+            // 
+            num0.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            num0.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            num0.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            num0.Location = new System.Drawing.Point(751, 36);
+            num0.Name = "num0";
+            num0.Size = new System.Drawing.Size(75, 75);
+            num0.TabIndex = 10;
+            num0.Text = ")\r\n0";
+            num0.UseVisualStyleBackColor = false;
+            num0.Click += new System.EventHandler(this.num0_Click);
+            // 
+            // button7
+            // 
+            button7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            button7.Location = new System.Drawing.Point(526, 36);
+            button7.Name = "button7";
+            button7.Size = new System.Drawing.Size(75, 75);
+            button7.TabIndex = 11;
+            button7.Text = "&\r\n7";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // cmdShift
+            // 
+            this.cmdShift.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cmdShift.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cmdShift.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdShift.ForeColor = System.Drawing.SystemColors.Control;
+            this.cmdShift.Location = new System.Drawing.Point(76, 182);
+            this.cmdShift.Name = "cmdShift";
+            this.cmdShift.Size = new System.Drawing.Size(150, 75);
+            this.cmdShift.TabIndex = 12;
+            this.cmdShift.Text = "Shift";
+            this.cmdShift.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.cmdShift.UseVisualStyleBackColor = false;
+            this.cmdShift.CheckedChanged += new System.EventHandler(this.cmdShift_CheckedChanged);
+            // 
+            // cmdCaps
+            // 
+            this.cmdCaps.Appearance = System.Windows.Forms.Appearance.Button;
+            this.cmdCaps.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cmdCaps.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdCaps.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cmdCaps.Location = new System.Drawing.Point(76, 106);
+            this.cmdCaps.Name = "cmdCaps";
+            this.cmdCaps.Size = new System.Drawing.Size(130, 75);
+            this.cmdCaps.TabIndex = 13;
+            this.cmdCaps.Text = "Caps\r\n    Lock";
+            this.cmdCaps.UseVisualStyleBackColor = false;
+            this.cmdCaps.CheckedChanged += new System.EventHandler(this.cmdCaps_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1372, 410);
+            this.Controls.Add(this.cmdCaps);
+            this.Controls.Add(this.cmdShift);
+            this.Controls.Add(button7);
+            this.Controls.Add(num0);
+            this.Controls.Add(num9);
+            this.Controls.Add(num8);
+            this.Controls.Add(num6);
+            this.Controls.Add(num5);
+            this.Controls.Add(num4);
+            this.Controls.Add(num2);
+            this.Controls.Add(num3);
+            this.Controls.Add(num1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.CheckBox cmdShift;
+        private System.Windows.Forms.CheckBox cmdCaps;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams param = base.CreateParams;
+                param.ExStyle |= 0x08000000;
+                return param;
+
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void num1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("∮");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("{∫}");
+        }
+
+        private void cmdCaps_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked)
+                cmdCaps.ForeColor = System.Drawing.Color.Red;
+            else
+                cmdCaps.ForeColor = SystemColors.ButtonHighlight;
+
+        }
+
+        private void cmdShift_CheckedChanged(object sender, EventArgs e)
+        {
+            
+              if((!(cmdCaps.Checked))&&(cmdShift.Checked))  
+                  cmdShift.ForeColor = System.Drawing.Color.Red;
+            else
+                cmdShift.ForeColor = SystemColors.ButtonHighlight;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num2_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("@");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("2");
+        }
+
+        private void num3_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("#");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("3");
+        }
+
+        private void num4_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("$");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("4");
+        }
+
+        private void num5_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("{%}");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("5");
+        }
+
+        private void num6_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("{^}");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("&");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("7");
+        }
+
+        private void num8_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("*");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("8");
+        }
+
+        private void num9_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("{(}");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("9");
+        }
+
+        private void num0_Click(object sender, EventArgs e)
+        {
+            if (cmdCaps.Checked || cmdShift.Checked)
+            {
+                SendKeys.Send("{)}");
+                cmdShift.Checked = false;
+            }
+            else
+                SendKeys.Send("0");
+        }
+
+ 
+    }
+}
